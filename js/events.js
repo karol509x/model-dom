8c48b6a0f81a4ff52b6f97f8fca4f7bb0a88e265
karol509x <karol509x@gmail.com>
2017-08-20 15:09:16 +0200

    eventy

--- js/events.js
+++ js/events.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function klikHeader() {
+    console.log('Kliknąłeś w header');
+ }
+
+function alarm(event) {
+    event.preventDefault(); //zapobiegamy domyslnej akcji
+    console.log('Kliknieto link');
+    console.log(event.type);
+}
+
+var secondLink = document.getElementsByTagName('a')[1];
+secondLink.onclick = alarm;
+
+document.getElementById('title-header').onclick = klikHeader; // wywołaj funkcje na elemencie  o id 'title-header'
+
+var paragraf = document.getElementsByTagName('p')[0];
+
+paragraf.addEventListener('click', klikHeader);
